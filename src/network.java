7f40b5dcfff1255d7e12a20eec86c00e73bb7fee
Mack Zahn <mackenzie.zahn@uoit.net>
2019-03-22 16:33:48 -0400

    first commit

---
+++ src/network.java
@@ -0,0 +1,203 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+public class network {
+
+    public static void main(String[] args){
+        //bitmap inputs
+        double[][] X = {
+                            {0,0,1,0,0,0,1,1,0,0,1,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0},
+                            {0,1,1,1,0,1,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1,1,1,1,1},
+                            {0,1,1,1,0,1,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1,1,0,0,0,1,0,1,1,1,0},
+                            {0,0,0,1,0,0,0,1,1,0,0,0,1,1,0,0,1,0,1,0,0,1,0,1,0,1,0,0,1,0,1,1,1,1,1,0,0,0,1,0,0,0,0,1,0},
+                            {1,1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,1,1,1,0,1,0,0,0,1,0,0,0,0,1,0,0,0,0,1,1,0,0,0,1,0,1,1,1,0},
+                            {0,1,1,1,0,1,0,0,0,1,1,0,0,0,0,1,0,0,0,0,1,1,1,1,0,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0},
+                            {1,1,1,1,1,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0},
+                            {0,1,1,1,0,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0},
+                            {0,1,1,1,0,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,0,1,1,1,1,0,0,0,0,1,0,0,0,0,1,1,0,0,0,1,0,1,1,1,0},
+                            {0,1,1,1,0,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0},
+                        };
+        //Expected outputs
+        double[][] Y = {
+                            {1,0,0,0,0,0,0,0,0,0},
+                            {0,1,0,0,0,0,0,0,0,0},
+                            {0,0,1,0,0,0,0,0,0,0},
+                            {0,0,0,1,0,0,0,0,0,0},
+                            {0,0,0,0,1,0,0,0,0,0},
+                            {0,0,0,0,0,1,0,0,0,0},
+                            {0,0,0,0,0,0,1,0,0,0},
+                            {0,0,0,0,0,0,0,1,0,0},
+                            {0,0,0,0,0,0,0,0,1,0},
+                            {0,0,0,0,0,0,0,0,0,1}
+                        };
+
+        //X = np.T(X);
+        //Y = np.T(Y);
+
+        int nSamples = 10;
+        int nInputs = 45;
+        int nodes = 5;
+        int nOutputs = 10;
+        int epocs = 20000;
+        double learnRate = 0.01;
+
+        
+        //layer 1 weights (nimputs columns, nodes rows)
+        //mth row is mth input, nth column is nth node
+        double[][] W1 = np.random(nInputs, nodes);
+        double[][] b1 = new double[nSamples][nodes];
+
+        //layer 2 (output layer) weights
+        double[][] W2 = np.random(nodes, nOutputs);
+        double[][] b2 = new double[nSamples][nOutputs];
+
+
+        //START LOOP HERE
+        
+        List<double[][]> toPlot = new ArrayList<double[][]>();
+        for(int q = 0; q<epocs;q++){
+            
+            //apply weights layer 1
+            double[][] sumL1 = np.add(np.dot(X,W1), b1);
+
+            //apply sigmoid layer 1
+            double[][] Z1 = np.sigmoid(sumL1);
+
+            // printMatrix(sumL1, nSamples, nodes);
+            // printMatrix(Z1, nSamples, nodes);
+
+
+            //apply weights layer 2
+            double[][] sumL2 = np.add(np.dot(Z1,W2),b2);
+            double[][] Z2 = np.sigmoid(sumL2);
+
+            // printMatrix(sumL2, 1, 9);
+            // printMatrix(Z2, 1, 9);
+
+            //BACK PROPaGATION
+
+            //Layer 2
+            double[][] deltaE2 = np.subtract(Z2, Y);
+
+            double[][] dW2 = np.divide(np.dot(np.T(Z1), deltaE2), nSamples);
+
+            double[][] db2 = np.divide(deltaE2, nSamples);
+
+
+            //double[][] deltaW2 = np.dot(np.multiply(learnRate, Z1),deltaE2);
+            //W2 = np.add(deltaW2, W2);
+
+            //layer 1
+            double[][] deltaE1 = np.multiply(np.dot(deltaE2, np.T(W2)), np.subtract(1.0,np.power(Z1, 2)));
+
+            double[][] dW1 = np.divide(np.dot(np.T(X),deltaE1),nSamples);
+
+            double[][] db1 = np.divide(deltaE1, nSamples);
+
+            W1 = np.subtract(W1, np.multiply(learnRate, dW1));
+            b1 = np.subtract(b1, np.multiply(learnRate, db1));
+
+            W2 = np.subtract(W2, np.multiply(learnRate, dW2));
+            b2 = np.subtract(b2, np.multiply(learnRate, db2));
+
+            if(q % (epocs/10) == 0){
+            	toPlot.add(new double[][]{
+            		{(double)q, SSE(deltaE2[0])},
+            		{(double)q, SSE(deltaE2[1])},
+            		{(double)q, SSE(deltaE2[2])},
+            		{(double)q, SSE(deltaE2[3])},
+            		{(double)q, SSE(deltaE2[4])},
+            		{(double)q, SSE(deltaE2[5])},
+            		{(double)q, SSE(deltaE2[6])},
+            		{(double)q, SSE(deltaE2[7])},
+            		{(double)q, SSE(deltaE2[8])},
+            		{(double)q, SSE(deltaE2[9])},
+            	});
+            	
+            	/*toPlot.add(new double[][]{
+            		{(double)q, SSE(Z2[0])},
+            		{(double)q, SSE(Z2[1])},
+            		{(double)q, SSE(Z2[2])},
+            		{(double)q, SSE(Z2[3])},
+            		{(double)q, SSE(Z2[4])},
+            		{(double)q, SSE(Z2[5])},
+            		{(double)q, SSE(Z2[6])},
+            		{(double)q, SSE(Z2[7])},
+            		{(double)q, SSE(Z2[8])},
+            		{(double)q, SSE(Z2[9])},
+            	});*/
+            	
+               /* System.out.println("****************");
+                printMatrix(deltaE2);
+                System.out.println("");
+                printMatrix(Z2);*/
+            }
+
+        }
+        List<double[]> newPoints = new ArrayList<double[]>();
+        List<double[][]> readyToPlot= new ArrayList<double[][]>();
+        
+        for(int i = 0; i<toPlot.get(i).length-1;i++){
+        	newPoints = new ArrayList<double[]>();
+        	for(int j=0;j<toPlot.size();j++){
+        		newPoints.add(toPlot.get(j)[i]);
+        	}
+        	readyToPlot.add( new double[][]{
+        				newPoints.get(0),
+        				newPoints.get(1),
+        				newPoints.get(2),
+        				newPoints.get(3),
+        				newPoints.get(4),
+        				newPoints.get(5),
+        				newPoints.get(6),
+        				newPoints.get(7),
+        				newPoints.get(8),
+        				newPoints.get(9)
+        	});
+        }
+        
+        for(int i=0;i<readyToPlot.size();i++){
+        	final int index = i;
+            SwingUtilities.invokeLater(() -> {
+      	      plot example = new plot("Number: "+index, readyToPlot.get(index));
+      	      example.setSize(800, 400);
+      	      example.setLocationRelativeTo(null);
+      	      example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+      	      example.setVisible(true);
+      	    });
+        }
+       
+        
+        
+    }
+
+    public static void printMatrix(double[][] mat){
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j< mat[i].length; j++){
+                System.out.print(mat[i][j] + ", ");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public static double SSE(double[] mat){
+    	double sum = 0;
+    	for(int i = 0; i<mat.length;i++){
+    		sum += mat[i];
+    	}
+    	
+    	double mean = sum/(mat.length);
+    	sum = 0;
+    	for(int i = 0; i<mat.length;i++){
+			sum += Math.pow((mat[i] - mean),2);
+    	}
+    	
+    	return sum;
+    	
+    }
+
+}
---
+++ src/plot.java
@@ -0,0 +1,55 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+
+public class plot extends JFrame {
+	  private static final long serialVersionUID = 6294689542092367723L;
+
+	  public plot(String title, double[][] mat) {
+	    super(title);
+
+	    // Create dataset
+	    XYDataset dataset = createDataset(mat);
+
+	    // Create chart
+	    JFreeChart chart = ChartFactory.createXYLineChart(
+	        "SSE vs Iteration",
+	        "Iteration",
+	        "SSE",
+	        dataset,
+	        PlotOrientation.VERTICAL,
+	        true, true, false);
+
+	    // Create Panel
+	    ChartPanel panel = new ChartPanel(chart);
+	    setContentPane(panel);
+	  }
+
+	  private XYDataset createDataset(double[][] mat) {
+	    XYSeriesCollection dataset = new XYSeriesCollection();
+
+
+	    XYSeries series = new XYSeries("trendline");
+	    
+	    for(int i = 0; i< mat.length;i++){
+	    	System.out.println(mat[i][0]);
+	    	System.out.println(mat[i][1]);
+	    	System.out.println("**********************************************");
+	    	series.add(mat[i][0], mat[i][1]);
+	    }
+
+	    //Add series to dataset
+	    dataset.addSeries(series);
+	    
+	    return dataset;
+	  }
+	}
